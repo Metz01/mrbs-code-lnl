dafdf44fa3b7e7993da8ef01017f8d50cc7c1223
Matteo Camillo <36533886+Metz01@users.noreply.github.com>
2023-12-11 12:11:32 +0100

    Update config.inc.php

--- web/config.inc.php
+++ web/config.inc.php
@@ -1,4 +1,22 @@
 <?php
+// $Id: config.inc.php 2799 2014-01-09 12:44:22Z cimorrison $
+
+/**************************************************************************
+ *   MRBS Configuration File
+ *   Configure this file for your site.
+ *   You shouldn't have to modify anything outside this file.
+ *
+ *   This file has already been populated with the minimum set of configuration
+ *   variables that you will need to change to get your system up and running.
+ *   If you want to change any of the other settings in systemdefaults.inc.php
+ *   or areadefaults.inc.php, then copy the relevant lines into this file
+ *   and edit them here.   This file will override the default settings and
+ *   when you upgrade to a new version of MRBS the config file is preserved.
+ **************************************************************************/
+
+/******
+ * LDAP
+ ******/
 
 $auth["type"] = "ldap";
 
@@ -17,20 +35,19 @@ $ldap_base_dn = "dc=lnl,dc=infn,dc=it";
 // Attribute within the base dn that contains the username
 $ldap_user_attrib = "uid";
 
-// $Id: config.inc.php 2799 2014-01-09 12:44:22Z cimorrison $
 
-/**************************************************************************
- *   MRBS Configuration File
- *   Configure this file for your site.
- *   You shouldn't have to modify anything outside this file.
- *
- *   This file has already been populated with the minimum set of configuration
- *   variables that you will need to change to get your system up and running.
- *   If you want to change any of the other settings in systemdefaults.inc.php
- *   or areadefaults.inc.php, then copy the relevant lines into this file
- *   and edit them here.   This file will override the default settings and
- *   when you upgrade to a new version of MRBS the config file is preserved.
- **************************************************************************/
+/************
+ * USER ROLES
+ ************/
+
+$auth['deny_public_access'] = TRUE;
+
+$max_level = 3;
+$min_booking_admin_level = 3;
+
+unset($auth["admin"]);
+$auth["admin"][] = "mcamillo";
+$auth["user"][] = "dlupu";
 
 /**********
  * Timezone
@@ -82,28 +99,3 @@ $db_password = 'mrbs';
 $db_tbl_prefix = "mrbs_";
 // Uncomment this to NOT use PHP persistent (pooled) database connections:
 // $db_nopersist = 1;
-
-
-/* Add lines from systemdefaults.inc.php and areadefaults.inc.php below here
-   to change the default configuration. Do _NOT_ modify systemdefaults.inc.php
-   or areadefaults.inc.php.  */
-
-#define('admin', ['mcamillo']);
-
-
-#$mrbs_admin = "Servizio Tecnologie Informatiche";
-#$mrbs_admin_email = "calcolo@lnl.infn.it";
-
-#$mrbs_company = "INFN LNL";
-#$mrbs_company_url = "http://www.lnl.infn.it";
-
-$auth['deny_public_access'] = TRUE;
-
-$max_level = 3;
-$min_booking_admin_level = 3;
-
-unset($auth["admin"]);
-$auth["admin"][] = "127.0.0.1";
-$auth["admin"][] = "admin";
-$auth["admin"][] = "mcamillo";
-$auth["user"][] = "dlupu";
